b7519441aa3172e2df1e3bbb890a66143cd079b5
zxcCookies <kirillsamsonov079@gmail.com>
2023-11-16 15:11:08 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>App</div>
+  let A = {
+    name: "VACI"
+    email:[vasya@gmaol.com]
+  }
+  let list = [
+  "alex",
+  {
+name:"...."
+email:"........"
+  }]
+</template>
---
+++ src/header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>Header</div>
+</template>
+<script setup>
+
+</script>
